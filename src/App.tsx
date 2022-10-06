770eecea309122d9b3899b9d5a4168a742b6c79e
Pedro Alquimim <pedrohalquimim@gmail.com>
2022-10-06 00:14:35 -0300

    created pages and router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from './Router'
 import { defaultTheme } from './styles/themes/default'
 import { ThemeProvider } from "styled-components"
 import { GlobalStyles } from './styles/global'
 
+
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <GlobalStyles />
+      <BrowserRouter>
+        <Router />
+        <GlobalStyles />
+      </BrowserRouter>
     </ThemeProvider>
   )
 }
---
+++ src/Router.tsx
@@ -0,0 +1,14 @@
+import { Route, Routes } from "react-router-dom";
+import { Checkout } from "./pages/Checkout";
+import { Home } from "./pages/Home";
+import { Success } from "./pages/Success";
+
+export function Router() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/checkout" element={<Checkout />} />
+      <Route path="/success" element={<Success />} />
+    </Routes>
+  )
+}
