7c4a6da1436182e8887fd2421925bcc38e34571f
Pedro Alquimim <pedrohalquimim@gmail.com>
2022-10-08 17:00:51 -0300

    first intro styles done

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,48 @@
-import { Header } from "../../components/Header";
+import { ShoppingCart } from "phosphor-react";
+import coffeeImage from '../../assets/coffeeImage.svg'
+import {
+  IntroContainer,
+  IntroDescriptionContainer,
+  TitleContainer,
+  InfoIntroItems
+} from "./style"
 
 export function Home() {
   return (
-    <div></div>
+    <div>
+      <IntroContainer>
+        <IntroDescriptionContainer>
+          <TitleContainer>
+            <h1>Encontre o café perfeito para qualquer hora do dia</h1>
+            <p>Com o Coffee Delivery você recebe seu café onde estiver, a qualquer hora</p>
+          </TitleContainer>
+
+          <InfoIntroItems>
+            <div>
+              <ShoppingCart size={22} weight='fill'/>
+              <p>Compra simples e segura</p>
+            </div>
+            <div>
+              <ShoppingCart size={22} weight='fill' />
+              <p>Compra simples e segura</p>
+            </div>
+            <div>
+              <ShoppingCart size={22} weight='fill' />
+              <p>Compra simples e segura</p>
+            </div>
+            <div>
+              <ShoppingCart size={22} weight='fill' />
+              <p>Compra simples e segura</p>
+            </div>
+          </InfoIntroItems>
+        </IntroDescriptionContainer>
+
+        <img src={coffeeImage} />
+      </IntroContainer>
+
+      <section>
+        <h2>Nossos cafés</h2>
+      </section>
+    </div>
   )
 }
--- src/pages/Home/style.ts
+++ src/pages/Home/style.ts
@@ -1,1 +1,47 @@
-import styled from "styled-components";
+import styled from "styled-components"
+
+export const IntroContainer = styled.section`
+  display: flex;
+  justify-content: center;
+  gap: 3.5rem;
+  margin-top: 5.75rem;
+  margin-bottom: 5.75rem;
+`
+
+export const IntroDescriptionContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 4.125rem;
+`
+
+export const TitleContainer = styled.div`
+  gap: 1rem;
+
+  h1 {
+    font-family: 'Baloo 2', sans-serif;
+    font-size: 3rem;
+    font-weight: 800;
+    line-height: 130%;
+
+    color: ${(props) => props.theme['base-title']};
+  }
+
+  p {
+    font-size: 1.25rem;
+    line-height: 130%;
+
+    color: ${(props) => props.theme['base-subtitle']};
+  }
+`
+
+export const InfoIntroItems = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 2.5rem;
+
+  div {
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+  }
+`
