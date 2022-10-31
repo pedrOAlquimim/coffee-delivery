ce62fab93a09bfd865e3461ce0602f338982fde3
Pedro Alquimim <pedrohalquimim@gmail.com>
2022-10-31 09:05:58 -0300

    Checkout page component: started

--- src/pages/Checkout/index.tsx
+++ src/pages/Checkout/index.tsx
@@ -1,3 +1,49 @@
+import { MapPinLine } from 'phosphor-react'
+import {
+  CheckoutContainer,
+  Title,
+  FormContainer,
+  FormHeader,
+  Form
+} from './styles'
+
 export function Checkout() {
-  return <h1>Checkout</h1>
+  return (
+    <CheckoutContainer>
+      <div>
+        <Title>Complete seu pedido</Title>
+        <FormContainer>
+          <FormHeader>
+            <span>
+              <MapPinLine />
+            </span>
+            <div>
+              <h4>Endereço de Entrega</h4>
+              <p>Informe o endereço onde deseja receber seu pedido</p>
+            </div>
+          </FormHeader>
+
+          <form>
+            <Form>
+              <input type="number" />
+              <input type="text" />
+              <input type="number" />
+              <input type="text" />
+              <input type="text" />
+              <input type="text" />
+              <input type="text" />
+            </Form>
+          </form>
+        </FormContainer>
+
+        <div>
+
+        </div>
+      </div>
+
+      <div>
+        <h3>Cafés selecionados</h3>
+      </div>
+    </CheckoutContainer>
+  )
 }
